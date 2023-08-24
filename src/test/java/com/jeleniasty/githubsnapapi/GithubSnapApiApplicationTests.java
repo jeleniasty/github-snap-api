85b0074a539389c846d32204d1ae7785e2b15bd4
micha <michal.jeleniasty@gmail.com>
2023-08-24 17:53:01 +0200

    Initial commit

---
+++ src/test/java/com/jeleniasty/githubsnapapi/GithubSnapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.githubsnapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSnapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
